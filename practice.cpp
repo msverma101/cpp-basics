f18b85380780dac7727c70705b610e96a0909a68
Sneha Verma <snehaverma@pop-os.localdomain>
2021-05-26 15:51:36 +0200

    first commit

---
+++ practice.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main( )
+{
+    cout << "the size of char is " << sizeof(char) << endl;
+    cout << "the size of bool is " << sizeof(bool) << endl;
+    cout << "the size of int " << sizeof(int) << endl;
+    cout << "the size of short " << sizeof(short) << endl;
+    cout << "the size of long " << sizeof(long) << endl;
+    cout << "the size of long long" << sizeof(long long) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of long double " << sizeof(long double) << endl;
+    cout << "the size of long long double " << sizeof(long long double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+    cout << "the size of double " << sizeof(double) << endl;
+
+}
